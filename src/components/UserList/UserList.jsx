bebeea4d90f204785fdbdcb3151c4e8b1145e8d8
p3javier <65142395+p3javier@users.noreply.github.com>
2021-01-26 00:44:10 +0100

    improvements to the lifecycle handling to improve performance and avoid an infinite loop of API calls

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from "react";
 import "./UserList.css";
 import users from "../../Services/userApi";
-
+const getUsers = async () => {
+  const usersResponse = await users().then((response) => response);
+  return usersResponse;
+};
 function UserList(props) {
   const [userList, setUserList] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
-    users().then((response) => setUserList(response));
-    const results = userList.filter((user) =>
-      user.name.toLowerCase().includes(props.searchTerm)
-    );
-    setSearchResults(results);
+    getUsers().then((res) => setUserList(res));
+  }, [props]);
+
+  useEffect(() => {
+    //users().then((response) => setUserList(response));
+    //let reResponse = getUsers();
+    if (userList.length) {
+      const results = userList.filter((user) =>
+        user.name.toLowerCase().includes(props.searchTerm)
+      );
+      setSearchResults(results);
+    }
   }, [userList, props]);
   const data = searchResults;
   return (
