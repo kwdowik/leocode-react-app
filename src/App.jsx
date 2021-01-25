ed6364a9fb079f7052dab658c08dde68f428af06
p3javier <65142395+p3javier@users.noreply.github.com>
2021-01-25 19:15:13 +0100

    Handle change function added to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.css";
 import UserList from "./components/UserList/UserList.jsx";
+import React, { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  //This function is going to be fired each time a change event is detected in the input field
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Users list</h1>
       </header>
-      <input type="text" placeholder="Search by user name..." />
+      <input
+        type="text"
+        placeholder="Search by user name..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
       <UserList />
     </div>
   );
